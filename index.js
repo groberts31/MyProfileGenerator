fb275574e80221b47937ca15a0aff4eba2a38921
Greg Roberts <groberts3104@gmail.com>
2019-12-16 18:50:15 -0500

    added function back to pull from github and console.log name to index.html and a pdf file

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-const questions = [
-  
-];
+const fs = require("fs");
+const axios = require("axios");
+const inquirer = require("inquirer");
+const util = require("util");
+var pdf = require("pdf-creator-node");
 
-function writeToFile(fileName, data) {
- 
-}
+const readFileAsync = util.promisify(fs.readFile);
 
-function init() {
 
-init();
+
+inquirer
+    .prompt([
+        {
+            message: "Enter your GitHub username:",
+            name: "username"
+        },
+        {
+            message: "What is your favorite color from options below?",
+            type: "list",
+            name: "colorOps",
+            choices: ["Red", "Blue", "Green", "Yellow", "Orange"]
+        }])
+    .then(function({ username }) {
+        const queryUrl = `https://api.github.com/users/${username}`;
+
+        axios.get(queryUrl).then(function(res) {
+            const name = res.data.name;
+            console.log(name);
+            fs.appendFileSync('profile.pdf', name);
+            fs.appendFileSync('index.html', name);
+            console.log(res.data.name);
+      
+        });
+    });
